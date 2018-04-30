a1c6386bad0af79f4764bbddc6c40f5edc94ca01
fateevv85 <fateevv85@gmail.com>
2018-04-30 17:34:53 +0300

    hw5

--- public/index.php
+++ public/index.php
@@ -2,27 +2,28 @@
 
 include $_SERVER['DOCUMENT_ROOT'] . "/config/main.php";
 include ROOT_DIR . "/services/Autoloader.php";
+include VENDOR_DIR . '/autoload.php';
 
 spl_autoload_register([new app\services\Autoloader(), 'loadClass']);
 
 $controllerName = $_GET['c'] ?: 'product';
 $actionName = $_GET['a'];
 
-$controllerClass = CONTROLLERS_NAMESPACE . ucfirst($controllerName) . 'Controller';
-//var_dump($_GET);
-//http://php-ii/public/index.php?c=product&action=catalog
-//var_dump($controllerClass);
-//http://php-ii/public/index.php?c=product&a=card&id=1
-if(class_exists($controllerClass)) {
-    $controller = new $controllerClass;
+$controllerClass = CONTROLLERS_NAMESPACE .
+    ucfirst($controllerName) . 'Controller';
+
+if (class_exists($controllerClass)) {
+    $controller = new $controllerClass(
+        new \app\services\TemplateRenderer()
+    );
     $controller->runAction($actionName);
 }
 
-//$product = \app\models\Product::getOne(66, 1);
+//$product = \app\models\Product::getOne(13, 1);
 //var_dump($product);
 //var_dump(\app\models\Product::getAll(1));
 /*$product1 = new app\models\Product(
-    66,
+    14,
     'Big_Book',
     '2',
     1,
@@ -32,9 +33,11 @@ if(class_exists($controllerClass)) {
     'picture_small',
     'picture'
 );*/
-//$product->price = 222;
+//var_dump($product1);
+//$product->price = 777;
+//$product->description = 'SS-SS';
 //$product->description = 'OOOOO';
-//$product1->save();
+//$product->save();
 //var_dump(\app\models\Product::getAll());
 //var_dump(\app\models\Product::deleteStrings([22,23,24]));
 //$product->delete();
